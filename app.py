69163b6b256c4899e73f37f2277212f53761630c
Gourav Dhawan <gouravdhawan@MacBookPro.lan>
2025-11-24 21:08:08 +0530

    Added refresh tokens

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token,jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
 
 import sqlite3
 
@@ -10,7 +11,10 @@ app = Flask(__name__)
 # --- Config ---
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # change in production
+app.config['JWT_SECRET_KEY'] = 'super-secret-key' 
+app.config['JWT_TOKEN_LOCATION']= ['headers']
+app.config['JWT_ACCESS_TOKEN_EXPIRES']= timedelta(minutes=15)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(dats=30)
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
@@ -65,8 +69,22 @@ def login():
     if not user or not check_password_hash(user.password, password):
         return jsonify({'message': 'Invalid credentials'}), 401
 
-    token = create_access_token(identity=str(user.id))
-    return jsonify({'access_token': token}), 200
+    access_token = create_access_token(identity=str(user.id))
+    refresh_token = create_refresh_token(identity=user.id, fresh = True)
+    return jsonify({'access_token': access_token,
+                    'refresh token': refresh_token
+                    }), 200
+##Refresh token route
+@app.route('/api/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    user_id = get_jwt_identity()
+    new_access_token = create_access_token(identity=user_id, fresh=False)
+    return jsonify({'access_token': new_access_token}),200
+
+
+
+
 
 ## Get all notes for current user
 @app.route('/api/notes', methods=['GET'])
